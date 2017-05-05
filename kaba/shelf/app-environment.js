562b7273911b867f2d3913c67233bde4bc1c7176
Jannik Zschiesche <jz@becklyn.com>
2017-05-05 18:51:18 +0200

    Add basic app environment for shelf tasks

---
+++ kaba/shelf/app-environment.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // Whether to build for debug
+    debug: false,
+    // Whether to lint the files
+    lint: false,
+    // Whether to start the watcher
+    watch: false,
+    // Whether to run in verbose mode
+    verbose: false,
+    // What to do when running a shelf task
+    mode: "compile",
+};
