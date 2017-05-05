fef944212a7144e31eed07ea745a71cb0a161f9e
Jannik Zschiesche <jz@becklyn.com>
2017-05-05 17:02:10 +0200

    Refactor main kaba class as ES class

--- kaba/kaba.js
+++ kaba/kaba.js
@@ -1,212 +1,198 @@
-"use strict";
-
-const inherits = require('util').inherits;
-const EventEmitter = require('events').EventEmitter;
+const EventEmitter = require('events');
 const async = require("async");
 
-// Define Kaba class
-function Kaba ()
-{
-    EventEmitter.call(this);
-    this.tasks = {};
-    this.taskCounter = 0;
 
-    /**
-     * @const
-     * @type {string}
-     */
-    this.DEFAULT_TASK_NAME = "";
-}
+// export a single instance
+module.exports = new Kaba();
 
-inherits(Kaba, EventEmitter);
 
 /**
- * Sets an entry in the registry
- *
- * @private
- * @param {string} taskName
- * @param {Function} taskFunction
+ * Main Kaba class
  */
-Kaba.prototype.set = function (taskName, taskFunction)
+class Kaba extends EventEmitter
 {
-    // normalize null to default task name
-    if (null === taskName)
+    constructor ()
     {
-        taskName = this.DEFAULT_TASK_NAME;
+        super();
+
+        /**
+         * The list of all defined tasks
+         *
+         * @private
+         * @type {{}}
+         */
+        this.tasks = {};
+
+        /**
+         * The name of the default task
+         *
+         * @private
+         * @type {string}
+         */
+        this.DEFAULT_TASK_NAME = "";
+
+
+        // define default tasks
+        this.shelf = {
+            scss: require("./kaba/scss"),
+            js: require("./kaba/js")
+        };
     }
 
-    if (this.tasks[taskName])
+
+    /**
+     * Returns the registered task by name
+     *
+     * @private
+     * @param {string} taskName
+     * @returns {Function|undefined}
+     */
+    get (taskName)
     {
-        if (this.DEFAULT_TASK_NAME === taskName)
+        return this.tasks[this.normalizeTaskName(taskName)];
+    }
+
+
+    /**
+     * Sets an entry in the registry
+     *
+     * @private
+     * @param {?string} taskName
+     * @param {Function} taskFunction
+     */
+    task (taskName, taskFunction)
+    {
+        taskName = this.normalizeTaskName(taskName);
+
+        if (this.tasks[taskName])
         {
-            throw new Error("Default task is already defined.");
+            if (this.DEFAULT_TASK_NAME === taskName)
+            {
+                throw new Error(`Default task is already defined.`);
+            }
+            else
+            {
+                throw new Error(`Task with name ${taskName} already defined.`);
+            }
         }
-        else
+
+        if (typeof taskFunction !== "function")
         {
-            throw new Error("Task with name " + taskName + " already defined.");
+            throw new Error(`Only functions can be registered as tasks, ${typeof taskFunction} given.`);
         }
-    }
 
-    if (typeof taskFunction !== "function")
-    {
-        throw new Error("Only functions can be registered as tasks, " + typeof taskFunction + " given.");
-    }
+        const taskId = this.listTasks().length;
 
-    let taskId = this.taskCounter;
-    this.taskCounter += 1;
-
-    // generate a new task function, that properly emits the required timing events
-    this.tasks[taskName] = (done, ...taskArguments) => {
-        this.emit("start", {
-            task: taskName,
-            id: taskId
-        });
-
-        let taskDone = () => {
-            this.emit("end", {
+        // generate a new task function, that properly emits the required timing events
+        this.tasks[taskName] = (done, ...taskArguments) => {
+            this.emit("start", {
                 task: taskName,
                 id: taskId
             });
-            done();
+
+            const taskDone = () => {
+                this.emit("end", {
+                    task: taskName,
+                    id: taskId
+                });
+                done();
+            };
+
+            // run task
+            taskFunction(taskDone, ...taskArguments);
         };
-
-        // run task
-        taskFunction(taskDone, ...taskArguments);
-    };
-};
-
-
-/**
- * Returns the registered task by name
- *
- * @private
- * @param {string} taskName
- * @returns {Function|undefined}
- */
-Kaba.prototype.get = function (taskName)
-{
-    // normalize null to default task name
-    if (null === taskName)
-    {
-        taskName = this.DEFAULT_TASK_NAME;
     }
 
-    return this.tasks[taskName];
-};
 
-
-/**
- * Registers or fetches a task by name.
- *
- * If no callback is given, the task with the given name is returned.
- * If a callback is give, the task is registered with the given name.
- *
- * @param {?string} taskName
- * @param {?function} callback
- * @returns {function|undefined}
- */
-Kaba.prototype.task = function (taskName, callback = null)
-{
-    if (!callback)
+    /**
+     * Returns a list of all registered tasks
+     *
+     * @returns {string[]}
+     */
+    listTasks ()
     {
-        return this.get(taskName);
+        return Object.keys(this.tasks);
     }
 
-    return this.set(taskName, callback);
-};
-
-/**
- * Returns a list of all registered tasks
- *
- * @returns {string[]}
- */
-Kaba.prototype.listTasks = function ()
-{
-    return Object.keys(this.tasks);
-};
 
 
-/**
- * Transforms a list of task functions (function or name of the task)
- * into a list of callable functions
- *
- * @private
- * @param {Array.<string|function>} taskFunctions
- * @returns {Array.<function>}
- */
-Kaba.prototype.normalizeTaskFunctions = function (taskFunctions)
-{
-    return taskFunctions.map(
-        (fn) => (typeof fn === "function") ? fn : this.get(fn)
-    );
-};
-
-
-/**
- * Prepares the task functions to be proper arguments for async.series and async.parallel calls
- *
- * @private
- * @param {Array.<function>} taskFunctions
- * @param {Array} callArguments
- * @returns {Array<function>}
- */
-Kaba.prototype.prepareTaskFunctionsForAsync = function (taskFunctions, callArguments)
-{
-    return taskFunctions.map(
-        /**
-         *
-         * @param {Function} fn
-         */
-        (fn) => {
-            return function(asyncCallback)
-            {
-                fn(asyncCallback, ...callArguments);
-            }
-        }
-    );
-};
-
-
-/**
- * Defines a wrapper functions that calls the given tasks ins series
- *
- * @returns {Function}
- */
-Kaba.prototype.series = function (...taskFunctions)
-{
-    let normalizedTaskFunctions = this.normalizeTaskFunctions(taskFunctions);
-
-    return (globalDone, ...callArguments) =>
+    /**
+     * Transforms a list of task functions (function or name of the task)
+     * into a list of their callable functions.
+     *
+     * @private
+     * @param {Array.<string|function>} taskFunctions
+     * @returns {Array.<function>}
+     */
+    getTaskFunctions (taskFunctions)
     {
-        let transformedFunctions = this.prepareTaskFunctionsForAsync(normalizedTaskFunctions, callArguments);
-        return async.series(transformedFunctions, globalDone);
-    };
-};
+        return taskFunctions.map(
+            (fn) => (typeof fn === "function") ? fn : this.get(fn)
+        );
+    }
 
 
-/**
- * Defines a wrapper functions that calls the given tasks in parallel
- *
- * @returns {Function}
- */
-Kaba.prototype.parallel = function (...taskFunctions)
-{
-    let normalizedTaskFunctions = this.normalizeTaskFunctions(taskFunctions);
-
-    return (globalDone, ...callArguments) =>
+    /**
+     * Prepares the task functions to be proper arguments for async.series and async.parallel calls
+     *
+     * @private
+     * @param {Array.<function>} taskFunctions
+     * @param {Array.<*>} callArguments
+     * @returns {Array.<function>}
+     */
+    prepareTaskFunctionsForAsync (taskFunctions, callArguments)
     {
-        let transformedFunctions = this.prepareTaskFunctionsForAsync(normalizedTaskFunctions, callArguments);
-        return async.parallel(transformedFunctions, globalDone);
-    };
-};
+        return taskFunctions.map(
+            (fn) => (asyncCallback) => fn(asyncCallback, ...callArguments)
+        );
+    }
 
 
+    /**
+     * Defines a wrapper functions that calls the given tasks in series
+     *
+     * @returns {function}
+     */
+    series (...tasks)
+    {
+        let taskFunctions = this.getTaskFunctions(tasks);
 
-// define default tasks
-Kaba.prototype.shelf = {
-    scss: require("./kaba/scss"),
-    js: require("./kaba/js")
-};
+        return (done, ...callArguments) =>
+        {
+            let asyncFunctions = this.prepareTaskFunctionsForAsync(taskFunctions, callArguments);
+            return async.series(asyncFunctions, done);
+        };
+    }
 
 
-module.exports = new Kaba();
+    /**
+     * Defines a wrapper functions that calls the given tasks in parallel
+     *
+     * @returns {function}
+     */
+    parallel (...tasks)
+    {
+        let taskFunctions = this.getTaskFunctions(tasks);
+
+        return (globalDone, ...callArguments) =>
+        {
+            let asyncFunctions = this.prepareTaskFunctionsForAsync(taskFunctions, callArguments);
+            return async.parallel(asyncFunctions, globalDone);
+        };
+    }
+
+
+    /**
+     * Normalizes the given task name, by optionally transforming it to the default task name
+     *
+     * @private
+     * @param {string} taskName
+     * @returns {string}
+     */
+    normalizeTaskName (taskName)
+    {
+        return (null !== taskName)
+            ? taskName
+            : this.DEFAULT_TASK_NAME;
+    }
+}
