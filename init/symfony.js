d89131b7a83007f1e89f1617ecab02066a626f44
Jannik Zschiesche <hello@apfelbox.net>
2017-07-19 22:10:43 +0200

    Reference tasks by name, to have better logging

--- init/symfony.js
+++ init/symfony.js
@@ -6,5 +6,5 @@ const scss = kaba.shelf.scss();
 kaba.task("scss", scss);
 kaba.task("js", js);
 
-kaba.task("", kaba.parallel(scss, js));
+kaba.task("", kaba.parallel("scss", "js"));
 kaba.task("release", kaba.parallel(""));
