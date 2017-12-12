8b5f90ad138f0a28f27293dc423dbefc97a8b3da
Jannik Zschiesche <jz@becklyn.com>
2017-12-12 15:44:54 +0100

    Add bin header

--- bin/kaba.js
+++ bin/kaba.js
@@ -1,5 +1,13 @@
 #!/usr/bin/env node
 
+const chalk = require("chalk");
+
+
+console.log(``);
+console.log(`${chalk.black(chalk.bgYellow("  ~~~~~~~~~  "))}`);
+console.log(`${chalk.black(chalk.bgYellow("   🍫️ kaba   "))}`);
+console.log(`${chalk.black(chalk.bgYellow("  ~~~~~~~~~  "))}`);
+console.log(``);
 
 console.log('Running webpack ...');
 console.log();
