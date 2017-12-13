dc50e4e3f41129601ff4954b171aff31333d9439
Jannik Zschiesche <jz@becklyn.com>
2017-12-13 18:34:40 +0100

    Refactor command line parsing

--- bin/run.js
+++ bin/run.js
@@ -1,35 +1,48 @@
 #!/usr/bin/env node
 
 const chalk = require("chalk");
-const cli = require("./cli");
+const program = require("commander");
+const Versions = require("../lib/versions");
 
 console.log(``);
-console.log(`${chalk.black(chalk.bgYellow("  ~~~~~~~~~~  "))}`);
-console.log(`${chalk.black(chalk.bgYellow("   🍫  kaba    "))}`);
-console.log(`${chalk.black(chalk.bgYellow("  ~~~~~~~~~~  "))}`);
+console.log(`  ${chalk.black(chalk.bgYellow("  ~~~~~~~~~~  "))}`);
+console.log(`  ${chalk.black(chalk.bgYellow("   🍫  kaba    "))}`);
+console.log(`  ${chalk.black(chalk.bgYellow("  ~~~~~~~~~~  "))}`);
 console.log(``);
 
-if (cli.showHelp())
+program
+    .option('--dev, -d', 'enables debug, file watchers, linting and source maps')
+    .option('--debug', 'enables debug builds (non-minified and with env `development`)')
+    .option('--with-source-maps', 'outputs source maps')
+    .option('--watch', 'starts the file watcher')
+    .option('--lint', 'lints all compiled files')
+    .option('--verbose', 'show all errors in the runner / config file with stack trace')
+    .option('--version, -V', 'output version info')
+    .parse(process.argv);
+
+
+if (program.version)
 {
-    // @todo implement
-    console.log("show help");
-    process.exit(0);
-}
-else if (cli.showVersion())
-{
-    // @todo implement
-    console.log("show version");
+    const versions = new Versions({
+        kaba: "yellow",
+        "kaba-babel-preset": "yellow",
+        webpack: "cyan",
+        "babel-core": "blue",
+        "node-sass": "blue",
+        "typescript": "blue",
+        "eslint": "blue",
+        "stylelint": "blue",
+    });
+    versions.print();
     process.exit(0);
 }
 
-// set environment
-process.env.NODE_ENV = cli.isDebug() ? '"development"' : '"production"';
-
-// strip all other arguments
-process.argv = process.argv.slice(0,2);
 
 try
 {
+    // strip all other arguments to not confuse webpack
+    process.argv = process.argv.slice(0,2);
+
     console.log(chalk`Running {cyan webpack} ...`);
     console.log();
     require('webpack/bin/webpack');
@@ -38,7 +51,7 @@ catch (e)
 {
     console.log(chalk`{red Webpack Error: ${e.message}}`);
 
-    if (cli.isVerbose())
+    if (program.verbose)
     {
         console.error(e);
     }
