9278dda4da08c920659e12dcdc2518a2e0cad48f
Jannik Zschiesche <jz@becklyn.com>
2019-09-24 15:24:55 +0200

    Display build progress

--- src/Kaba.ts
+++ src/Kaba.ts
@@ -60,6 +60,7 @@ export class Kaba
         this.cwd = process.cwd();
         this.libRoot = path.dirname(__dirname);
         this.plugins = [
+            new webpack.ProgressPlugin(),
             new ProvidePlugin({
                 h: ["preact", "h"],
             }),
